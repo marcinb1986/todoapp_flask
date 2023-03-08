145ff505534e4412d14939b4d1f80025a802f2b0
Marcin Banaszkiewicz <marcin.banaszkiewicz@300brains.com>
2023-03-08 21:32:58 +0100

    first commit backend

--- models/tag.py
+++ models/tag.py
@@ -8,10 +8,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # class Tag(Base):
+
+
 class TagModel(db.Model):
     __tablename__ = 'tags'
 
     id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String)
+    name = db.Column(db.String)
     action_id = db.Column(db.Integer, db.ForeignKey('actions.id'))
     action = db.relationship("Action", uselist=False, lazy='dynamic')
--- schemas.py
+++ schemas.py
@@ -5,21 +5,24 @@ class PersonsSchema(Schema):
     name = fields.Str(required=True)
     lastName = fields.Str(required=True)
 
+
 class TagsSchema(Schema):
     id = fields.Int(required=True)
     name = fields.Str(required=True)
 
-class ActionsSchema(PersonsSchema, TagsSchema):
-    id= fields.Int(required=True)
-    description=fields.Str(required=True)
+
+class ActionsSchema(Schema):
+    id = fields.Int(required=True)
+    description = fields.Str(required=True)
     action = fields.Str(required=True)
     tag = fields.List(fields.Nested(TagsSchema(), required=True))
-    category= fields.Str(required=True)
+    category = fields.Str(required=True)
     persons = fields.List(fields.Nested(PersonsSchema()))
 
-class ActionSchema(TagsSchema):
-    id=fields.Int(required=True)
-    description=fields.Str(required=True)
+
+class ActionSchema(Schema):
+    id = fields.Int(required=True)
+    description = fields.Str(required=True)
     action = fields.Str(required=True)
-    tag = fields.List(fields.Nested(TagsSchema(), required = True))
+    tag = fields.List(fields.Nested(TagsSchema(), many=True))
     category = fields.Str(required=True)
